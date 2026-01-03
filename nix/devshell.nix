26c265004f757defb8f0c77406c49c1460dde4fe
DaveVED <me@dsqr.dev>
2026-01-02 22:56:55 -0600

    chore: init template

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -11,35 +11,16 @@ in
   # Create a development shell
   devShells.${system}.default = pkgs.mkShell {
     buildInputs = with pkgs; [
-      curl
-      wget
-      just
       nixfmt-rfc-style
       nixfmt-tree
       statix
       deadnix
       nil
       bun
-      starship
     ];
 
     shellHook = ''
-      if [[ -n "$ZSH_VERSION" ]]; then
-        eval "$(starship init zsh)"
-      else
-        eval "$(starship init bash)"
-      fi
-
       echo "Bun version: $(bun --version)"
-      echo "🚀 Development shell activated, you can now compile things"
-
-      scaffold() {
-        bun run scaffold "$@"
-      }
     '';
-
-    # Prefer zsh as the shell
-    preferLocalBuild = true;
-    shell = "${pkgs.zsh}/bin/zsh";
   };
 }
